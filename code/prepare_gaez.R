5b6b798e03de3a6c2542d26495cc23bd1f43b2cd
valentinguye <valentin.guye@ens-paris-saclay.fr>
2021-03-24 10:00:48 +0100

    First commit

---
+++ code/prepare_gaez.R
@@ -0,0 +1,85 @@
+
+##### 0. PACKAGES, WD, OBJECTS #####
+
+
+### WORKING DIRECTORY SHOULD BE CORRECT IF THIS SCRIPT IS RUN WITHIN R_project_for_individual_runs
+### OR CALLED FROM LUCFP PROJECT master.do FILE.
+### IN ANY CASE IT SHOULD BE (~/LUCFP/data_processing) 
+
+
+### PACKAGES ###
+# see this project's README for a better understanding of how packages are handled in this project. 
+
+# These are the packages needed in this particular script. 
+neededPackages = c("dplyr", "here", "readstata13", 
+                   "raster", "rgdal", "sp", "sf",
+                   "doParallel", "foreach", "parallel")
+
+# Install them in their project-specific versions
+renv::restore(packages = neededPackages)
+
+# Load them 
+lapply(neededPackages, library, character.only = TRUE)
+
+# /!\/!\ IF renv::restore(neededPackages) FAILS TO INSTALL SOME PACKAGES /!\/!\ 
+
+# For instance sf could cause trouble https://github.com/r-spatial/sf/issues/921 
+# or magick, as a dependency of raster and rgdal. 
+
+# FOLLOW THESE STEPS:
+# 1. Remove these package names from neededPackages above, and rerun renv::restore(packages = neededPackages)
+# 2. Write them in troublePackages below, uncomment, and run the following code chunk: 
+
+# # /!\ THIS BREAKS THE PROJECT REPRODUCIBILITY GUARANTY /!\
+# troublePackages <- c("")
+# # Attempt to load packages from user's default libraries. 
+# lapply(troublePackages, library, lib.loc = default_libraries, character.only = TRUE)
+
+# 3. If the troubling packages could not be loaded ("there is no package called ‘’") 
+#   you should try to install them, preferably in their versions stated in the renv.lock file. 
+#   see in particular https://rstudio.github.io/renv/articles/renv.html 
+
+
+
+### NEW FOLDERS USED IN THIS SCRIPT 
+# dir.create(here("temp_data", "GAEZ", "AES_index_value", "Rain-fed", "High-input"), recursive = TRUE) 
+# dir.create(here("temp_data", "GAEZ", "AES_index_value_current", "Rain-fed", "High-input"), recursive = TRUE) 
+
+datadir <- here("input_data", "GAEZ", "AES_index_value", "Rain-fed", "High-input")
+targetdir <- here("temp_data", "GAEZ", "AES_index_value", "Rain-fed", "High-input")
+tmpdir <- here("temp_data", "tmp")
+
+if (!dir.exists(tmpdir)) dir.create(tmpdir, recursive = TRUE)
+if (dir.exists(targetdir)) {
+  file.remove(list.files(path = targetdir,
+                         pattern = ".tif", full.names = TRUE))
+} else dir.create(targetdir, recursive = TRUE)
+
+
+files <- list.files(path = datadir, pattern = ".zip")
+crops <- unlist(strsplit(files, split = ".zip"))
+
+ext <- extent(c(-180, 179.9167, -30, 30))
+
+## Import most crops
+for (j in 1:length(files)) {
+  #if (any(crops[j] == cropsToAggregate)) next
+  print(files[j])
+  unzip(zipfile = here(datadir, files[j]), exdir = tmpdir)
+  dt <- raster(paste0(tmpdir, "/data.asc"))
+  
+  dt_trop <- crop(dt, ext)
+  dt_trop[dt_trop<0] <- NA # A few points are -0.09 with no apparent reason. 
+  
+  names(dt_trop) <- crops[j]
+  writeRaster(dt_trop,
+              filename = here(targetdir, paste0(crops[j], ".tif")),
+              overwrite = TRUE)
+  
+}
+
+
+
+
+
+
